7852b01d5a9d0d8c8c4788b85ea18f33867e9af1
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-04-02 18:55:41 -0300

    Add AddItemActivity

--- WeDeployWebinar/app/src/main/java/com/victorlaerte/wedeploywebinar/AddItemActivity.kt
+++ WeDeployWebinar/app/src/main/java/com/victorlaerte/wedeploywebinar/AddItemActivity.kt
@@ -2,6 +2,8 @@ package com.victorlaerte.wedeploywebinar
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 
 /**
@@ -14,5 +16,8 @@ class AddItemActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_item_activity)
 
         val container = findViewById<LinearLayout>(R.id.content)
+//        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+//        val descriptionEditText = findViewById<EditText>(R.id.descriptionEditText)
+//        val addButton = findViewById<Button>(R.id.addButton)
     }
 }
